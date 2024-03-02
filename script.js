fe34250d0dc1ea9d1d3f47767a6bd8fe9b39511e
tucjpg <161941618+tucjpg@users.noreply.github.com>
2024-03-02 14:50:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('heart-button').addEventListener('click', function() {
+    document.getElementById('message-popup').style.display = 'block';
+});
+
+document.getElementById('close-button').addEventListener('click', function() {
+    document.getElementById('message-popup').style.display = 'none';
+});
